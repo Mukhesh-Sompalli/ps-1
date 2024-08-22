b596d6ec03b04187a181bf1a16f18e39877a809e
Mukhesh-Sompalli <mukheshsompalli02@gmail.com>
2024-08-23 00:23:21 +0530

    add

---
+++ index.js
@@ -0,0 +1,126 @@
+// let age = 18;  
+// if (age) {  
+// console.log("You are eligible to vote.");  
+// }  
+// if (age >= 18) {  
+// console.log("You are eligible to vote.");  
+// }  
+// else {  
+// console.log("You are not eligible to vote.");  
+// } 
+
+
+// let marks = 75;  
+// if (marks >= 90) {  
+// console.log("Grade A");  
+// } else if (marks < 80) {  
+// console.log("Grade B");  
+// } else if (marks == 70) {  
+// console.log("Grade C");  
+// } else {  
+// console.log("Needs improvement");  
+// } 
+
+
+
+
+// let loginStatus = true;  
+// let accountBalance = 500;  
+// if (loginStatus) {  
+// if (accountBalance > 0) {  
+// console.log("You can make a purchase.");  
+// } else {  
+// console.log("Insufficient balance.");  
+// }  
+// } else {  
+// console.log("Please log in to continue.");  
+// } 
+
+
+
+// let j = 0;  
+// do {  
+// console.log(`Number ${j}`);  
+// j++;  
+// } while (j < 5 && j++);  
+
+
+// let dayoff = false;  
+// let holiday = true;  
+// if (dayoff || holiday) {  
+// console.log("You can relax today.");  
+// } else {  
+// console.log("It's a working day.");  
+// } 
+
+
+
+// for (let i = 0; i < 10; i++) {  
+//     if (i === 2*i){  
+//     break;
+//     }  
+//     console.log(`Number ${i}`);  
+//     } 
+
+
+
+// for (let i = 1; i <= 3; i++) {  
+//     for (let j = 1; j <= 3; j++) {  
+//     if(i<2)  
+//     console.log(`i = ${i}, j = ${j}`);  
+//     }  
+//     } 
+
+
+
+
+// for (let i = 5; i >= 0; i=i-2) {  
+//     console.log(`Countdown: ${i}`);  
+//     } 
+
+
+// let count = 0;  
+// while (true) {  
+// count++;  
+// console.log(`Count: ${count}`);  
+// if (count === 5)  
+// break;  
+// } 
+
+
+
+
+// let grade = 'B';  
+// switch (grade) {  
+// case 'A':  
+// case 'B':  
+// case 'C':  
+// console.log("You passed!");  
+// break;  
+// default:  
+// console.log("You failed.");  
+// } 
+
+
+
+
+// num=prompt("enter a number")
+// c=0
+// min=num[0]
+// if(num>1){
+// for(i=2;i<num.length;i++){
+//     if(num%i==0){
+//         c++
+//         break
+//     }
+
+//     if(c==0)
+//     if(num[i]<min){
+//         min=num[i]
+//     }
+// }
+// }
+// console.log(min)
+
+
+
